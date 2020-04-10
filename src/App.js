e5dc2b41a4edd8c8c700b57d2007659f5f4f1edf
mdemora <miguel.uni.demora@gmai.com>
2020-04-11 01:42:45 +0200

    Inicial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import CakeContainer from './components/cakeContainer';
+import {Provider} from 'react-redux'
+import store from './redux/store'
+import HooksCakeContainer from './components/hooksCakeContainer';
+import IceCreamContainer from './components/iceCreamContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer/>
+        <CakeContainer/>
+        <IceCreamContainer/>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
